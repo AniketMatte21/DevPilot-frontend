a8234faa8f5b05e1e8ba72b0b08a99f470e5d403
GauriUplanchiwar <uplanchiwargauri@gmail.com>
2026-09-03 16:08:22 +0530

    changes added

--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -982,7 +982,8 @@ const handleChat = (repo) => {
               type="button"
               size="sm"
               className="w-full sm:w-auto gap-2"
-              onClick={() => startIndexingLogicHere(repo)}
+              repo={repo}
+              onError={setIndexError}
             >
               Start Indexing
               <ArrowRight className="h-4 w-4 shrink-0" />
